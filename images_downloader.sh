94859cc23317b9134c8aff794e22f03f90076739
Hikaru <auron0330@gmail.com>
2023-02-07 14:47:04 +0900

    fix sh

--- images_downloader.sh
+++ images_downloader.sh
@@ -2,7 +2,7 @@
 
 for var in 1 2 3 4 5 6 7 8 9
 do
-    curl -o ./app/assets/images/${var}.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/master/public/images/${var}.jpg
+    curl -o ./app/assets/images/${var}.jpg https://raw.githubusercontent.com/Auron0330/rails-fashion-ec-app/main/app/assets/images/${var}.jpg
 done
-curl -o ./app/assets/images/top.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/master/public/images/top.jpg
-curl -o ./app/assets/images/top2.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/master/public/images/top2.jpg
+curl -o ./app/assets/images/top.jpg https://raw.githubusercontent.com/Auron0330/rails-fashion-ec-app/main/app/assets/images/top.jpg
+curl -o ./app/assets/images/top2.jpg https://raw.githubusercontent.com/Auron0330/rails-fashion-ec-app/main/app/assets/images/top2.jpg
