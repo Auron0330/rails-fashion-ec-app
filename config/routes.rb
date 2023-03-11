edd7d47a71498eb43b758e6ed258394280fc44b8
Hikaru <auron0330@gmail.com>
2023-03-11 14:44:25 +0900

    add chapter-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "products#index"
 
   resources :products, only: [:show]
+
+  resources :line_items, only: %i[create destroy]
+
+  resources :carts, only: [:index]
 end
