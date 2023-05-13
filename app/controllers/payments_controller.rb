e65480b66c418a59a6070fb26eddcbc72b57a1f6
hinoue <auron0330@gmail.com>
2023-05-13 15:14:59 +0900

    refactoring

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,10 +2,8 @@ class PaymentsController < ApplicationController
   before_action :set_cart, only: %i[checkout success]
 
   def checkout
-    if @cart.line_items.empty?
-      redirect_to carts_path
-    else
-      @session = Stripe::Checkout::Session.create(StripeCheckout.make_stripe_checkout_params(cart: @cart))
+    redirect_to carts_path and return if @cart.line_items.empty?
+    @session = Stripe::Checkout::Session.create(StripeCheckout.make_stripe_checkout_params(cart: @cart))
   end
 
   def success
